8bb958c92ac1774216679ba3cb36d8aabe66e1d2
ezekiel-kaeyros <ezekiel.dombissi@kaeyros-analytics.com>
2024-07-11 10:08:48 +0100

    fix : landing page

--- landing-page/src/services/dataService.ts
+++ landing-page/src/services/dataService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL 
+export const API_URL = 'https://backcommunities.eze.ink';
 // export const API_URL = 'http://localhost:4000';
 
 export default class DataService {
